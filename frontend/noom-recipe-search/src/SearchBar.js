d48641121b67dea40eddb7de1eca1d566c84b542
JMoloughney <github email address>
2018-08-07 15:40:09 -0400

    recipe search works however does continually as user types-making multiple api calls

--- frontend/noom-recipe-search/src/SearchBar.js
+++ frontend/noom-recipe-search/src/SearchBar.js
@@ -3,7 +3,7 @@ import React from 'react'
 class SearchBar extends React.Component{
 
   state = {
-    search: "",
+    search: " ",
     recipes: []
   }
 
@@ -16,19 +16,22 @@ class SearchBar extends React.Component{
       .then(res => res.json())
       .then(recipes =>
         this.setState({
-          recipes: recipes
+          recipes: recipes.results
       }))
-      console.log(this.state.recipes.results)
+      console.log(this.state.recipes)
     }
 
   render(){
+    const recipes = this.state.recipes.map((recipe) => {
+      return <div className="recipe-card"> <h3 id="rec-title">{recipe.title}</h3> <h5 id='rec-link'>{recipe.href}</h5> {recipe.ingredients} </div>
+    });
     return (
       <div>
       <div className="searchbar-box">
-      <input name='search' id="searchbar-input" onChange={this.onChangeHandler} placeholder="Search Recipe"></input>
+        <input name='search' id="searchbar-input" onChange={this.onChangeHandler} placeholder="Search Keyword in Recipe"></input>
       </div>
       <div className='recipe-list'>
-      Recipes: {this.state.recipes.toString()}
+        {recipes}
       </div>
       </div>
 
